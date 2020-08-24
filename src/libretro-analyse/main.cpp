69b2668624888edf253e3092d0b543f19666420a
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2020-08-25 01:41:58 +0200

    libretro-analyse: added additional checks.

--- src/libretro-analyse/main.cpp
+++ src/libretro-analyse/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <optional>
+#include <set>
 
 #include <core-library.hpp>
 
 using namespace libretro;
-using namespace std;
+using namespace std::literals;
 
 auto title_box(std::string_view &&title, uint64_t spacer = 4) -> void {
 	std::string bar;
@@ -26,17 +27,17 @@ auto title_box(std::string_view &&title, uint64_t spacer = 4) -> void {
 	bar.front() = '+';
 	bar.back() = '+';
 
-	cout << endl
-		 << bar << endl
-		 << prefix << title << suffix << endl
-		 << bar << endl
-		 << endl;
+	std::cout << std::endl
+			  << bar << std::endl
+			  << prefix << title << suffix << std::endl
+			  << bar << std::endl
+			  << std::endl;
 }
 
 auto check_for_missing_signatures(const core_info_list &info_list, const core_list &cores) {
 	for (auto &&core : cores)
 		if (!find_core_info(core, info_list).has_value())
-			cerr << "missing: " << core.name.toStdString() << " has no info signature." << endl;
+			std::cerr << "missing: " << core.name.toStdString() << " has no info signature." << std::endl;
 }
 
 auto check_for_required_firmware(const core_info_list &info_list, const core_list &cores) {
@@ -44,7 +45,7 @@ auto check_for_required_firmware(const core_info_list &info_list, const core_lis
 		if (auto info = find_core_info(core, info_list); info.has_value())
 			for (auto &&fw_file : info->firmware)
 				if (!fw_file.is_found && !fw_file.is_optional)
-					cerr << "missing: " << core.name.toStdString() << " expects firmware: " << fw_file.path.toStdString() << endl;
+					std::cerr << "missing: " << core.name.toStdString() << " expects firmware: " << fw_file.path.toStdString() << std::endl;
 	}
 }
 
@@ -53,8 +54,25 @@ auto check_for_optional_firmware(const core_info_list &info_list, const core_lis
 		if (auto info = find_core_info(core, info_list); info.has_value())
 			for (auto &&fw_file : info->firmware)
 				if (!fw_file.is_found && fw_file.is_optional)
-					cerr << "missing: " << core.name.toStdString() << " expects firmware: " << fw_file.path.toStdString() << endl;
+					std::cerr << "missing: " << core.name.toStdString() << " expects firmware: " << fw_file.path.toStdString() << std::endl;
 	}
+}
+
+auto check_for_missing_systemid(const core_info_list &info_list) {
+	for (auto &&info : info_list)
+		if (info.systemid.isEmpty())
+			std::cerr << "missing: " << info.name.toStdString() << " have empty systemid." << std::endl;
+}
+
+auto check_for_supported_systems(const core_info_list &info_list) {
+	std::set<string> systems;
+
+	for (auto &&info : info_list)
+		if (!info.systemid.isEmpty())
+			systems.insert(info.systemid);
+
+	for (auto &&system : systems)
+		std::cout << system.toStdString() << std::endl;
 }
 
 auto main(int, char *[]) -> int {
@@ -64,19 +82,25 @@ auto main(int, char *[]) -> int {
 	auto &&info_list = library.info_list();
 
 	for (auto &&core : available_cores)
-		cout << "found: " << core.name.toStdString() << endl;
+		std::cout << "found: " << core.name.toStdString() << std::endl;
 
-	cout << available_cores.size() << " libretro cores found." << endl;
-	cout << info_list.size() << " libretro signatures available." << endl;
+	std::cout << available_cores.size() << " libretro cores found." << std::endl;
+	std::cout << info_list.size() << " libretro signatures available." << std::endl;
 
 	title_box("Missing info signatures"sv);
 	check_for_missing_signatures(info_list, available_cores);
+
+	title_box("Missing systemid's"sv);
+	check_for_missing_systemid(info_list);
 
 	title_box("Firmware requirements"sv);
 	check_for_required_firmware(info_list, available_cores);
 
 	title_box("Firmware requirements (optional firmware)"sv);
 	check_for_optional_firmware(info_list, available_cores);
+
+	title_box("Supported systems"sv);
+	check_for_supported_systems(info_list);
 
 	return 0;
 }
