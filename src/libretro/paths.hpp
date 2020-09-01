e213ccaea9be9d4fa7e2862be7eb82e31e66cf0a
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2020-09-01 23:37:33 +0200

    libretro: added assets path.

--- src/libretro/paths.hpp
+++ src/libretro/paths.hpp
@@ -4,6 +4,7 @@
 
 namespace libretro {
 
+constexpr auto assets_dir = LIBRETRO_ASSETS_PATH;
 constexpr auto core_dir = LIBRETRO_CORE_PATH;
 constexpr auto core_info_dir = LIBRETRO_CORE_INFO_PATH;
 constexpr auto firmware_dir = LIBRETRO_FIRMWARE_PATH;
